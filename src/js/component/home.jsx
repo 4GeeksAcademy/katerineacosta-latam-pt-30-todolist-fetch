5f743c27bc397bfcfe43b470da9a5f3c5e1ec6d9
kath424 <acostak424@gmail.com>
2024-06-08 06:23:45 +0000

    Agrega try y catch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,8 @@ import TodoList from "./todoList";
 import TodoInput from "./todoInput";
 import { useState } from "react";
 
+const user = 'kath';
+
 //create your first component
 const Home = () => { 
 	const [strList, setStrList] = useState([]);
@@ -16,76 +18,102 @@ const Home = () => {
 
 	const getAllTodos = async ()=>{
 		setError(null);
-		// try{
-		// 	const res = await fetch('https://playground.4geeks.com/todo/users/kaths', { method:'GET',  });
-		// 	const data = await res.json();
-		// 	if(res.status.toString()[0] != '2'){
-		// 		throw data;
-		// 	}
-		// 	setstrList(data.todos)		
-		// }catch (error){
-		// 	setstrList([]);
-		// 	console.log(error);
-		// 	setError(error.detail);
-		// }
+		let data = null;
+		try{
+			const res = await fetch(`https://playground.4geeks.com/todo/users/${user}`, { method:'GET' });
+			data  = await res.json();
 
-			const res = await fetch('https://playground.4geeks.com/todo/users/kath', { method:'GET' });
-			const data = await res.json();
-
-			// success
-			if(res.status.toString()[0] == '2'){
-				setStrList(data.todos);	
-			// user does not exists
-			}else if(res.status.toString()[0] == '4' && data.detail.includes("doesn't exist")){
-				createUser();
-				setStrList([]);
-			// other errors
-			}else{
-				setStrList([]);
-				console.log(error);
-				setError(error.detail);	
+			if(!res.ok){
+				console.log('error en respuesta', {res, data})
+				if(res.status.toString().startsWith('4') && data.detail.includes("doesn't exist")){
+					console.log('usuario no existe')
+					// user does not exists
+					await createUser();
+					setstrList([]);
+					return;
+				}else{
+					console.log("Error obteniendo To Do's")
+					throw "Error obteniendo To Do's";
+				}
 			}
+
+			setstrList(data.todos);	
+		}catch(exception){
+			console.log("Exception Catched 'getAllTodos'");
+			setstrList([]);
+			console.log({exception, data});
+			setError(data?.detail || exception || 'Ocurrio un error');
+		}
 	};
 
 	const addTodo = async (todoName) => {
 		if(todoName.trim() === ""){
 			return;
 		}
+		setError(null);
 		const body = JSON.stringify({
 			"label": todoName,
 			"is_done": false
 		});
-		const result = await fetch('https://playground.4geeks.com/todo/todos/kath', {
-			method: 'POST',
-			body: body,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		});
-		getAllTodos();
+		
+		try{
+			const res = await fetch(`https://playground.4geeks.com/todo/todos/${user}`, {
+				method: 'POST',
+				body: body,
+				headers: {
+					'Content-Type': 'application/json'
+				},
+			});
+			if(!res.ok){
+				console.log("Error creando usuario")
+				throw "Error creando usuario";
+			}
+
+			getAllTodos();
+		}
+		catch(exception){
+			console.log("Exception Catched 'add Todo'", );
+			setError(exception || 'Ocurrio un error al crear todo');
+		}
 	};
 
 	const removeTodo = async (todoId) => {
-		const result = await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {method:'DELETE'});
-		getAllTodos();
+		setError(null);
+		try{
+			const res = await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {method:'DELETE'});
+			if(!res.ok){
+				console.log('Error borrando ToDo: '+ todoId )
+				throw 'Error borrando ToDo: '+ todoId;
+			}
+			getAllTodos();
+		}catch(exception){
+			console.log("Exception Catched 'Delete Todo'", );
+			setError(exception || 'Error borrando ToDo');
+			throw exception;
+		}
+		
     };
 
 	const clearTodos = async () => {
-		for(let el of strList){
-			await fetch(`https://playground.4geeks.com/todo/todos/${el.id}`, {method:'DELETE'});
+		try{
+			for(let el of strList)
+				await removeTodo(el.id);
+		}catch(exception){
+			console.log("Exception Catched 'Clear ToDos'", );
+			setError(exception || 'Error borrando todos los ToDo');
 		}
+
 		getAllTodos();
 	};
 
 	const createUser = async ()=>{
-		const result = await fetch('https://playground.4geeks.com/todo/users/kath', {method:'POST'});
-		
-		/* success */
-		if( false ){
-
-		/* error */
-		}else{
-
+		try{
+			const res = await fetch(`https://playground.4geeks.com/todo/users/${user}`, {method:'POST'});
+			if(!res.ok){
+				throw "Error creando Usuario";
+			}
+		}catch(exception){
+			throw exception;
 		}
 	};
 
@@ -103,7 +131,7 @@ const Home = () => {
 						<div className="d-grid">
 							<button type="button" className="btn list__btn mt-2 " onClick={clearTodos} >Borrar todo</button>
 						</div>
-						{ error && (<div class="alert alert-danger" role="alert">
+						{ error && (<div className="alert alert-danger" role="alert">
 							{error}
 						</div>)
 						}
